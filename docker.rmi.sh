3261740229185df958bf2377f9308d0bdf61b268
Fux Peter <peter.fux@autopro24.at>
2024-07-05 00:03:20 +0200

    initialize project

--- docker.rmi.sh
+++ docker.rmi.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
-CADDY_IMAGE_NAME="a"
-NGINX_IMAGE_NAME="a"
-SEARCH="CADDY"
-ENV_NAME="${SEARCH}_IMAGE_NAME"
-IMAGE_NAME=$(eval echo \$$ENV_NAME)
-echo "name:${IMAGE_NAME}"
-exit 1
-
-
 source .env
 if [ -z "$1" ]; then
-  FILTER="draftmode/base."
+  FILTER="${DOCKER_NAMESPACE}/${IMAGE_PREFIX}*"
 else
-  FILTER="draftmode/-$1*"
+  FILTER="${DOCKER_NAMESPACE}/-$1*"
 fi
+echo ":$FILTER:"
 docker rmi -f $(docker images "$FILTER" -a -q)
